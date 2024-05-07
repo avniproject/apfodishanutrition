f7bae412a32735b24c821aded6e1c376721cadaf
Nupoor Khandelwal <37863085+nupoorkhandelwal@users.noreply.github.com>
2024-05-07 11:02:27 +0530

    Update "JSY money received" move from Delivery to 2nd PNC.sql

--- migrations/Phase 2/"JSY money received" move from Delivery to 2nd PNC.sql
+++ migrations/Phase 2/"JSY money received" move from Delivery to 2nd PNC.sql
@@ -6,6 +6,33 @@ set role apfodisha;
 
 begin transaction;
 
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Select query to get the count of 2nd PNC which will be updated from the script
+with pnc_data as (SELECT id,
+                         program_enrolment_id,
+                         observations,
+                         ROW_NUMBER()
+                         OVER (PARTITION BY pe.program_enrolment_id ORDER BY pe.earliest_visit_date_time asc NULLS LAST) AS visit_number
+                  FROM public.program_encounter pe
+                  WHERE pe.encounter_type_id = (SELECT id
+                                                FROM encounter_type et
+                                                WHERE et.name = 'PNC'
+                                                  AND et.is_voided = false)
+                    AND pe.encounter_date_time IS NOT NULL)
+select count(pnc.id)
+     from public.program_encounter as pnc
+     join public.program_encounter as delivery on pnc.program_enrolment_id = delivery.program_enrolment_id
+     join pnc_data on pnc_data.id = pnc.id
+where pnc_data.visit_number = 2
+  and pnc.program_enrolment_id = delivery.program_enrolment_id
+  and delivery.encounter_type_id = (select id from encounter_type where name = 'Delivery')
+  and (delivery.observations -> '3b6e00e3-b931-40aa-8af1-4416f7336650') is not null;
+
+
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- update query
+
 with pnc_data as (SELECT id,
                          program_enrolment_id,
                          observations,
