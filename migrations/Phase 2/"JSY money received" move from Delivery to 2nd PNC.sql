5b32f55450b7e336e1de0fb7053f73237991c419
Nupoor Khandelwal <37863085+nupoorkhandelwal@users.noreply.github.com>
2024-05-08 15:17:41 +0530

    Update "JSY money received" move from Delivery to 2nd PNC.sql

--- migrations/Phase 2/"JSY money received" move from Delivery to 2nd PNC.sql
+++ migrations/Phase 2/"JSY money received" move from Delivery to 2nd PNC.sql
@@ -52,9 +52,9 @@ SET observations = pnc.observations || jsonb_build_object('3b6e00e3-b931-40aa-8a
 	  last_modified_by_id = (select id from users where username = 'nupoork@apfodisha'),
 	  manual_update_history = append_manual_update_history(pnc.manual_update_history, 'Data migration from Delivery form to 2nd PNC #Phase 2')
 from public.program_encounter as delivery
-         join pnc_data on pnc_data.id = pnc.id
+         join pnc_data on pnc_data.program_enrolment_id = delivery.program_enrolment_id
 where pnc_data.visit_number = 2
-  and pnc.program_enrolment_id = delivery.program_enrolment_id
+  and pnc.id = pnc_data.id
   and delivery.encounter_type_id = (select id from encounter_type where name = 'Delivery')
   and (delivery.observations -> '3b6e00e3-b931-40aa-8af1-4416f7336650') is not null;
 
